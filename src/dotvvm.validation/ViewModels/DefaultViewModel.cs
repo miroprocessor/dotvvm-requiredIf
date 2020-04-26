92aa3107d4e87a91feec8a188b58aec4bf4b5334
Amir Ismail <eng.amir.ismail@gmai.com>
2020-04-26 03:31:19 +0200

    initial commit

---
+++ src/dotvvm.validation/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,26 @@
+﻿using dotvvm.validation.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotvvm.validation.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        [RequiredIf(nameof(Gender), "1", ErrorMessage = "Age is required")]
+        public int? Age { get; set; }
+
+        public DefaultViewModel()
+        {
+            Title = "Hello from DotVVM!";
+        }
+
+        public void Save()
+        { }
+    }
+}
